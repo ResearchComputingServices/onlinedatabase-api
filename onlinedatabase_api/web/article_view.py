6add9798cc792e097b15f968bd6e1bdd2ce6ca77
Kevin <kevin@MacBook-Pro.local>
2021-02-01 17:52:28 -0500

    add import and export feature

--- onlinedatabase_api/web/article_view.py
+++ onlinedatabase_api/web/article_view.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import request, send_file
 from flask import json, jsonify, Response, blueprints
 from onlinedatabase_api.models.role import Role, RoleSchema
 from onlinedatabase_api.models.article import Article, ArticleSchema
@@ -7,6 +7,9 @@ from onlinedatabase_api.web.common_view import onlinedatabase_bp
 from onlinedatabase_api.decorators.crossorigin import crossdomain
 from onlinedatabase_api.decorators.authentication import authentication
 from onlinedatabase_api.providers.article_provider import ArticleProvider
+import pandas as pd
+import math
+from io import BytesIO
 
 article_schema = ArticleSchema(many=False)
 article_schema_many = ArticleSchema(many=True)
@@ -103,4 +106,128 @@ def delete_article():
     except Exception as e:
         error = {"exception": str(e), "message": "Exception has occurred. Check the format of the request."}
         response = Response(json.dumps(error), 500, mimetype="application/json")
+    return response
+
+@onlinedatabase_bp.route("/articles/export", methods=['GET'])
+@crossdomain(origin='*')
+@authentication
+def export_articles():
+    specific_id = request.args.get('id')
+    if specific_id is None:
+        try:
+            records = []
+            articles = Article.query.all()
+            for i in range(len(articles)):
+                s_a = Article.query.filter_by(id=(i+1)).first()
+
+                records.append({
+                    "ID": s_a.id,
+                    "Title": s_a.name,
+                    "Author": s_a.author,
+                    "Publisher": s_a.publisher,
+                    "Year": s_a.year,
+                    "Language": s_a.language
+                })
+
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                pd.DataFrame(records).to_excel(writer,
+                                               sheet_name="articles",
+                                               index=False)
+                workbook = writer.book
+                worksheet = writer.sheets["articles"]
+                format = workbook.add_format()
+                format.set_align('center')
+                format.set_align('vcenter')
+                worksheet.set_column('A:A', 12, format)
+                worksheet.set_column('B:B', 38, format)
+                worksheet.set_column('C:C', 22, format)
+                worksheet.set_column('D:D', 38, format)
+                worksheet.set_column('E:E', 15, format)
+                worksheet.set_column('F:F', 18, format)
+                writer.save()
+            output.seek(0)
+            return send_file(output,
+                             attachment_filename= "Articles" + '.xlsx',
+                             mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                             as_attachment=True, cache_timeout=-1)
+        except Exception as e:
+            error = {"exception": str(e), "message": "Exception has occurred. Check the format of the request."}
+            response = Response(json.dumps(error), 404, mimetype="application/json")
+            return response
+
+    if specific_id is not None:
+        try:
+            name = request.args.get('name')
+            if name is None:
+                article_id = request.args.get('id')
+                s_a = Article.query.filter_by(id=article_id).first()
+                name = s_a.name
+                s_a = Article.query.filter_by(name=name).first()
+            else:
+                article_id = request.args.get('id')
+                s_a = Article.query.filter_by(id=article_id).first()
+
+            specific_user_info = [{
+                "ID": s_a.id,
+                "Title": s_a.name,
+                "Author": s_a.author,
+                "Publisher": s_a.publisher,
+                "Year": s_a.year,
+                "Language": s_a.language
+                }]
+
+            output = BytesIO()
+            with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+                pd.DataFrame(specific_user_info).to_excel(writer,
+                                               sheet_name="articles",
+                                               index=False)
+                workbook = writer.book
+                worksheet = writer.sheets["articles"]
+                format = workbook.add_format()
+                format.set_align('center')
+                format.set_align('vcenter')
+                worksheet.set_column('A:A', 12, format)
+                worksheet.set_column('B:B', 38, format)
+                worksheet.set_column('C:C', 22, format)
+                worksheet.set_column('D:D', 38, format)
+                worksheet.set_column('E:E', 15, format)
+                worksheet.set_column('F:F', 18, format)
+                writer.save()
+
+            output.seek(0)
+            return send_file(output,
+                             attachment_filename= "Articles" + '.xlsx',
+                             mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                             as_attachment=True, cache_timeout=-1)
+        except Exception as e:
+            response = Response(json.dumps(e), 404, mimetype="application/json")
+            return response
+
+@onlinedatabase_bp.route("/articles/upload", methods=['POST'])
+@crossdomain(origin='*')
+@authentication
+def upload_vocabulary():
+    raw_data = request.get_data()
+    data = pd.read_excel(raw_data, engine="openpyxl")
+    try:
+        for _, row in data.iterrows():
+            d = dict(row)
+            if type(d["Title"]) == str:
+                article = {
+                    "id": provider.generate_id(field=Article.id),
+                    "name": d["Title"],
+                    "author": d["Author"],
+                    "publisher": d["Publisher"],
+                    "year": d["Year"],
+                    "language": d["Language"],
+                }
+                articles = Article(article)
+                db.session.add(articles)
+        db.session.commit()
+        response = Response(json.dumps({"success": True}), 200, mimetype="application/json")
+    except Exception as e:
+        error = {"exception": str(e), "message": "Exception has occurred. Check the format of the request."}
+        response = Response(json.dumps(error), 500, mimetype="application/json")
+
     return response
